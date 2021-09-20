6080c00b21e411bc96b82729457364c763f6b396
abhishek-geek <2019005@iiitdmj.ac.in>
2021-09-21 00:10:58 +0530

    taining process

--- index.js
+++ index.js
@@ -1,31 +1,94 @@
-console.log('ml5 version:', ml5.version);
+// ml5.js: Pose Estimation with PoseNet
+// The Coding Train / Daniel Shiffman
+// https://thecodingtrain.com/learning/ml5/7.1-posenet.html
+// https://youtu.be/OIo-DIOkNVg
+// https://editor.p5js.org/codingtrain/sketches/ULA97pJXR
 
 let video;
-let posenet;
+let poseNet;
 let pose;
+let skeleton;
+let brain;
+let state = 'waiting';
+let targetLabel;
+
+function keyPressed() {
+  targetLabel = key;
+  console.log(targetLabel);
+  setTimeout(()=>{
+    console.log("collecting");
+    state = "collecting";
+    setTimeout(()=>{
+      console.log("not collecting");
+      state = "waiting";
+    }, 5000)
+  }, 5000)
+}
 
 function setup() {
   createCanvas(640, 480);
-  video = createCapture(VIDEO, ()=>console.log("Web cam is onn"));
+  video = createCapture(VIDEO);
   video.hide();
-  posenet = ml5.poseNet(video, ()=>console.log("PoseNet model loaded"));
-  posenet.on("pose", (poses)=>{
-    console.log("PoseNet is onn");
-    if(poses.length > 0) {
-      console.log("len is +ve");
-      pose = poses[0].poses;
+  poseNet = ml5.poseNet(video, modelLoaded);
+  poseNet.on('pose', gotPoses);
+  let options = {
+    inputs: 34,
+    outputs: 2,
+    task: 'classification',
+    debug: 'true'
+  };
+  brain = ml5.neuralNetwork(options);
+  background(255);
+}
+
+function gotPoses(poses) {
+  if (poses.length > 0) {
+    pose = poses[0].pose;
+    skeleton = poses[0].skeleton;
+    if(state = "collecting") {
+      let inputs = [];
+      for(let i=0; i<pose.keypoints.length; i++) {
+        let x = pose.keypoints[i].position.x;
+        let y = pose.keypoints[i].position.y;
+        inputs.push(x);
+        inputs.push(y);
+      }
+      let target = [targetLabel]
+      brain.addData(inputs, target);
     }
-  });
+  }
+}
+
+function modelLoaded() {
+  console.log('poseNet ready');
 }
 
 function draw() {
-  background(200);
   image(video, 0, 0);
-  if(pose) {
-    fill(255,0,0);
+
+  if (pose) {
+    let eyeR = pose.rightEye;
+    let eyeL = pose.leftEye;
+    let d = dist(eyeR.x, eyeR.y, eyeL.x, eyeL.y);
+    fill(255, 0, 0);
     ellipse(pose.nose.x, pose.nose.y, 10);
-    fill(0,0,255);
-    ellipse(pose.rightWrite.x, pose.rightWrist.y, 32);
+    fill(0, 0, 255);
+    ellipse(pose.rightWrist.x, pose.rightWrist.y, 32);
     ellipse(pose.leftWrist.x, pose.leftWrist.y, 32);
+
+    for (let i = 0; i < pose.keypoints.length; i++) {
+      let x = pose.keypoints[i].position.x;
+      let y = pose.keypoints[i].position.y;
+      fill(0, 255, 0);
+      ellipse(x, y, 16, 16);
+    }
+
+    for (let i = 0; i < skeleton.length; i++) {
+      let a = skeleton[i][0];
+      let b = skeleton[i][1];
+      strokeWeight(2);
+      stroke(255);
+      line(a.position.x, a.position.y, b.position.x, b.position.y);
+    }
   }
 }
